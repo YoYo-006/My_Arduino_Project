e8e1b83de5cb6fdf71411491f0ad6632fc49f5e6
Sudhang Balisseri <156525059+YoYo-006@users.noreply.github.com>
2025-05-18 23:31:07 +0530

    robotic-hand-first-commit

---
+++ Robotic_Hand/Robotic-Hand-Updated-Python-code/main.py
@@ -0,0 +1,17 @@
+import cvzone
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = cvzone.HandDetector(maxHands=1, detectionCon=int(0.7))
+mySerial = cvzone.SerialObject("COM3", 9600, 1)
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+    if lmList:
+        fingers = detector.fingersUp()
+        print(fingers)
+        mySerial.sendData(fingers)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+    cv2.VideoCapture
